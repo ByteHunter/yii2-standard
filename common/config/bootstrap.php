66d201cff77f07cb8db7dacb6a62368d6f64b4b0
TheByteHunter <rost18mlg@gmail.com>
2017-02-03 11:36:56 +0100

    Initial commit, starting template.

---
+++ common/config/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+Yii::setAlias('@common', dirname(__DIR__));
+Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
+Yii::setAlias('@panel', dirname(dirname(__DIR__)) . '/panel');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
